77e41e203b63eb0ce7f26effa47ded798dbc782e
Gabriel Ferreira Brito <ferreirabritogabriel@gmail.com>
2022-07-19 11:47:19 -0300

    Add button to Top and news styles on hover

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,25 @@
 function onScroll() {
+	showNavOnScroll();
+	showBackToTopOnScroll();
+}
+
+function showNavOnScroll() {
 	let navigationScroll = document.getElementById('navigation');
 
 	if (scrollY > 0) {
 		navigationScroll.classList.add('scroll');
 	} else {
 		navigationScroll.classList.remove('scroll');
+	}
+}
+
+function showBackToTopOnScroll() {
+	let backToTopScroll = document.getElementById('backToTop');
+
+	if (scrollY > 500) {
+		backToTopScroll.classList.add('show');
+	} else {
+		backToTopScroll.classList.remove('show');
 	}
 }
 
@@ -20,4 +35,4 @@ ScrollReveal({
 	origin: 'top',
 	distance: '30px',
 	duration: 700,
-}).reveal('#home, #home img, #home .stats, #services, #services header, #services .card');
+}).reveal('#home, #home img, #home .stats, #services, #services header, #services .card', '#about-us', '#about-us header', '#about-us .services');
