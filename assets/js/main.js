c4b76e60cb02ff9e147b2cbabc4dc456775491b4
Gabriel Ferreira Brito <ferreirabritogabriel@gmail.com>
2022-08-18 16:09:42 -0300

    Add limites topo e fundo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,38 @@
 function onScroll() {
 	showNavOnScroll();
 	showBackToTopOnScroll();
-	activateMenuAtCurrentSection();
+	activateMenuAtCurrentSection(services);
+	// activateMenuAtCurrentSection();
 }
 
 // Para marcar no header aonde estamos, destacando o campo que estamos scrollando no momento
-function activateMenuAtCurrentSection() {
+function activateMenuAtCurrentSection(section) {
+	// Linha imaginaria que sempre vai se localizar ao meio da pagina com ou sem o scroll, linha fixa ao meio da PAG
 	const targetLine = scrollY + innerHeight / 2;
 
 	// Verificar se o topo está acima da linha
-	const sectionTop = home.offsetTop;
-	const sectionHeight = home.offsetHeight;
+	const sectionTop = section.offsetTop; // offsetTop função nativa para indentificar o topo de uma seção, nesse caso é a home
+
+	const sectionHeight = section.offsetHeight; // offsetHeight função nativa para indentificar a altura da seção, que nos da um numero antes de começar a proxima seção
 
 	const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop;
 
-	console.log('O topo da sessão passou da linha? ' + sectionTopReachOrPassedTargetLine);
+	// Informações
+	console.log('O topo da sessão passou da linha? ', sectionTopReachOrPassedTargetLine);
 
 	// Verificar se a base está abaixo da linha
+	// Quais dados vou precisar?
+
+	// A sessão termina aonde?
+	const sectionEndsAt = sectionTop + sectionHeight;
+	console.log('sectionEndsAt', sectionEndsAt);
+
+	// O final da sessão passou da linha alvo?
+	const sectionEndPassedTargetLine = sectionEndsAt <= targetLine;
+
+	console.log(sectionEndPassedTargetLine);
+
+	// Limites da sessão
 }
 
 function showNavOnScroll() {
